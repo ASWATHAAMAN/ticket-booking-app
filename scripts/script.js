86df683ef99f169135419f139265f4a4b6f64bd8
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2023-06-17 20:40:29 +0530

    one step ahead

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,43 @@
 `use strict`;
+
+// select elements
+const moviePriceEl = document.getElementById(`movie-price`);
+const seatsEl = document.querySelectorAll(`.row .seats:not(.occupied)`);
+console.log(seatsEl);
+const countEl = document.getElementById(`count`);
+const totalEl = document.getElementById(`total`);
+// global variables
+let price;
+let count;
+let total;
+
+// functions
+function init() {
+  
+}
+
+// eventListeners
+moviePriceEl.addEventListener(`change`, () => {
+  Price = moviePriceEl.value;
+  console.log(Price);
+});
+
+for (let i = 0; i < seatsEl.length; i = i + 1) {
+  seatsEl[i].addEventListener(`click`, (Event) => {
+    if (Event.target.classList.contains(`selected`)) {
+      Event.target.classList.remove(`selected`);
+    } else {
+      Event.target.classList.add(`selected`);
+    }
+     seatCount = document.querySelectorAll(`.row .seats.selected`).length;
+    countEl.innerText = seatCount;
+
+    total = seatCount * price;
+    totalEl.innerText = total;
+    
+    
+  });
+}
+
+// initial settings
+init();
